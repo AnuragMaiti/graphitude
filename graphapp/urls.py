1b6991636878ca7664e8f48192d404edcc773766
Anurag Maiti <knoxxus@gmail.com>
2024-07-08 10:51:32 +0530

    Added csv view

--- graphapp/urls.py
+++ graphapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('csvuploader/', views.CsvUploader.as_view(), name='csvuploader'),
     # Add more URLs as needed
 ]
