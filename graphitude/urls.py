a4c4b5741cb4a416cddc3c91ed58b8396224fb24
Anurag Maiti <knoxxus@gmail.com>
2024-07-08 10:51:17 +0530

    Added layout/index page
    Signed-off-by: Anurag Maiti <konxxus@gmail.com>

--- graphitude/urls.py
+++ graphitude/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('graphapp/', include('graphapp.urls')),
+    path('admin/', admin.site.urls),
     # Add other URL patterns as needed
 ]
